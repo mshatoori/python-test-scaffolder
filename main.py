2d095fb5af6f42184ca12855f5f84884a22f82de
Mohammadreza MontazeriShatoori <monta2009@gmail.com>
2021-07-07 18:36:37 +0430

    Fix If split depth

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ class FoCSplitNode(FoCNode):
         super(FoCSplitNode, self).__init__(parent)
         self.branches = []
         self.state = 'new'
+        self.depth = 0
 
     def add_branch(self, node: Optional[cst.CSTNode]) -> FoCBranchNode:
         branch = FoCBranchNode(self)
@@ -104,8 +105,7 @@ class FunctionBodyVisitor(cst.CSTVisitor):
         return False
 
     def visit_If(self, node: "If") -> Optional[bool]:
-        self.chained_if_else_number += 1
-        print(f'Visiting if {code_gen(node.test)}', " | ", self.chained_if_else_number)
+        print(f'Visiting if {code_gen(node.test)}')
 
         top_split = self.top_split_node()
 
@@ -118,12 +118,22 @@ class FunctionBodyVisitor(cst.CSTVisitor):
             self.split_node_stack.append(if_split_node)
         else:
             assert top_split.state == 'or_else'
+            top_split.depth += 1
+
+
+        if top_split:
+            print(f'depth={top_split.depth}')
 
         return True
 
     def visit_If_body(self, node: "If") -> None:
         print(f'Visiting if body {code_gen(node.test)}')
         top_split = self.top_split_node()
+
+
+        if top_split:
+            print(f'depth={top_split.depth}')
+
         top_split.add_branch(node.body)
         top_split.state = 'in_body'
 
@@ -131,11 +141,14 @@ class FunctionBodyVisitor(cst.CSTVisitor):
         print(f'Leaving if body {code_gen(node.test)}')
 
     def visit_If_orelse(self, node: "If") -> None:
-        self.chained_if_else_number += 1
-        print(f'Visiting if orelse {code_gen(node.orelse) if node.orelse else "EMPTY"}', " | ", self.chained_if_else_number)
+        print(f'Visiting if orelse {code_gen(node.orelse) if node.orelse else "EMPTY"}')
 
         top_split = self.top_split_node()
         top_split.state = 'or_else'
+
+
+        if top_split:
+            print(f'depth={top_split.depth}')
 
         if node.orelse is None:
             top_split.add_branch(node.orelse)
@@ -145,18 +158,30 @@ class FunctionBodyVisitor(cst.CSTVisitor):
             branch = self.split_node_stack[-1].add_branch(node.orelse)
 
     def leave_If_orelse(self, node: "If") -> None:
-        self.chained_if_else_number -= 1
-        print(f'Leaving if orelse {code_gen(node.orelse) if node.orelse else "EMPTY"}', " | ", self.chained_if_else_number)
+        print(f'Leaving if orelse {code_gen(node.orelse) if node.orelse else "EMPTY"}')
         top_split = self.top_split_node()
         top_split.state = 'done'
 
+
+        if top_split:
+            print(f'depth={top_split.depth}')
+
     def leave_If(self, original_node: "If") -> None:
-        self.chained_if_else_number -= 1
-        print(f'Leaving if {code_gen(original_node.test)}', " | ", self.chained_if_else_number)
+        print(f'Leaving if {code_gen(original_node.test)}')
+
         top_split = self.top_split_node()
         assert top_split.state == 'done'
-        split_node = self.split_node_stack.pop()
-        # todo: add this split node to the tree
+
+
+        if top_split:
+            print(f'depth={top_split.depth}')
+
+        if top_split.depth == 0:
+            split_node = self.split_node_stack.pop()
+            # todo: add this split node to the tree
+            print(f'DELETE HERE: {len(split_node.branches)}')
+        else:
+            top_split.depth -= 1
 
 
 if __name__ == "__main__":
